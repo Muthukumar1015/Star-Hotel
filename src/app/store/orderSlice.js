f6525358b076de6a093dbc82bce81562edee2146
Muthukumar1015 <muthukumarshiva1031@gmail.com>
2025-03-28 15:42:46 +0530

    add order js

--- src/app/store/orderSlice.js
+++ src/app/store/orderSlice.js
@@ -1,18 +1,18 @@
-// import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// const orderSlice = createSlice({
-//   name: "orders",
-//   initialState: { orders: [] },
-//   reducers: {
-//     addOrder: (state, action) => {
-//       state.orders.push({ ...action.payload, status: "Order Confirmed" });
-//     },
-//     updateOrderStatus: (state, action) => {
-//       const order = state.orders.find((o) => o.id === action.payload.id);
-//       if (order) order.status = action.payload.status;
-//     },
-//   },
-// });
+const orderSlice = createSlice({
+  name: "orders",
+  initialState: { orders: [] },
+  reducers: {
+    addOrder: (state, action) => {
+      state.orders.push({ ...action.payload, status: "Order Confirmed" });
+    },
+    updateOrderStatus: (state, action) => {
+      const order = state.orders.find((o) => o.id === action.payload.id);
+      if (order) order.status = action.payload.status;
+    },
+  },
+});
 
-// export const { addOrder, updateOrderStatus } = orderSlice.actions;
-// export default orderSlice.reducer;
+export const { addOrder, updateOrderStatus } = orderSlice.actions;
+export default orderSlice.reducer;
